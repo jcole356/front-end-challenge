5c79f04d01d8a22fd4dcda460002cc202eb3e894
Joshua Cole <j_cole_356@yahoo.com>
2015-08-02 19:47:03 -0700

    finish front end challenge

--- script.js
+++ script.js
@@ -1,54 +1,100 @@
-
 function domobj(){
-  var self        =this;
+  var self        = this;
   self.products   = [];
 
   self.getproducts = function(url){
     $.getJSON(url, function(response){
-        for(i=0; i<response.sales.length ; i++){
-          self.products.push( new productobj(response.sales[i], i)  );
+        for(var i = 0; i < response.sales.length ; i++) {
+          self.products.push( new productobj(response.sales[i], i));
         }
     });
-  }
-    
+  };
+
   self.updateproducthtml = function(){
-    for( i=0; i< self.products.length ; i++){
+    for(var i = 0; i < self.products.length ; i++){
       self.products[i].updatehtml();
     }
-  }
-  
+  };
+
+  // Update the progress bar as the products are added.
   self.updatedom = function(){
-    var i=0
+    var i = 0;
+    var numProducts = self.products.length;
     thishtml='';
-    for( i=0; i< self.products.length ; i++){
-      if (i % 3 == 0 ){  thishtml += "<div class='row'>"; console.log("START") }
+    for( i = 0; i < numProducts ; i++){
       thishtml += self.products[i].htmlview;
-      if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";console.log("FINISH")}
+      var percent = ((i + 1) / numProducts) * 100;
+      self.progressBar(percent);
     }
-    $("#content").append(thishtml)
-  }
-  
+    $("#content").append(thishtml);
+    self.overlay();
+    self.removeProduct();
+  };
+
+  // Adds overlay to the product on mouseover
+  self.overlay = function () {
+    $(".product-container").mouseenter(function (){
+      var $product = $(event.currentTarget);
+      var overlay = $product.find(".overlay");
+      overlay.removeClass("hidden");
+    })
+    .mouseleave(function () {
+      var $product = $(event.currentTarget);
+      var overlay = $product.find(".overlay");
+      overlay.addClass("hidden");
+    });
+  };
+
+  // Add a progress bar until items are loaded into the dom
+  self.progressBar = function (percentComplete) {
+    var valueString = percentComplete + "%";
+    $progressBar = $(".progress-bar");
+    $progressBar.css({ "width" : valueString});
+  };
+
+  // Removes a product from the DOM when the x is clicked
+  self.removeProduct = function () {
+    $(".close").click(function () {
+      var $button = $(event.currentTarget);
+      var $product = $button.parent();
+      $product.remove();
+    });
+  };
 }
 
 function productobj(product, i){
   var self          = this;
-  self.photo        = product.photos.medium_half
-  self.title        = product.name
-  self.tagline      = product.tagline
-  self.url          = product.url
-  self.htmlview     = ""
-  self.index        = i
-  self.custom_class = "col"+ ((i % 3) +1)
-  
+  self.photo        = product.photos.medium_half;
+  self.title        = product.name;
+  self.tagline      = product.tagline;
+  self.url          = product.url;
+  self.htmlview     = "";
+  self.description  = product.description;
+
   self.updatehtml= function(){
     $.get('product-template.html', function(template){
-      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
+      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{description}', self.description).replace('{id}', self.id).replace('{custom_class}', self.custom_class);
     });
-  }
+  };
 }
 
+// Check for changes to the view port size
+$(window).resize(function () {
+  windowSize();
+});
 
-var page=new domobj();
+// Sets the width based on the viewport
+function windowSize() {
+  var width = $(window).width();
+  var bodyWidth = (Math.floor(width / 320) * 320);
+  $("#body").width(bodyWidth);
+}
+
+var page = new domobj();
+// Set initial viewport size
+windowSize();
 page.getproducts('data.json');
 setTimeout("console.log('building html');page.updateproducthtml();",20);
-setTimeout("page.updatedom()",50)
+setTimeout("page.updatedom()",500);
+// Removes progress bar after DOM has loaded
+setTimeout(function () {$(".progress-container").remove()}, 2000);
